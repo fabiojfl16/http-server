f2c18d73b9b9b3f091a353cce2ee3c65b968aaac
Joao Fabio <fabio.jfl16@gmail.com>
2025-05-29 17:35:44 -0300

    added multi thread execution for the server.

---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+use std::{
+    sync::{Arc, Mutex, mpsc},
+    thread,
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    /// Creates a new thread pool with the specified number of threads.
+    ///
+    /// The `size` parameter must be greater than zero.
+    ///
+    /// #Panics
+    ///
+    /// The new function will panic if the size is zero or less.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0, "Thread pool size must be greater than zero");
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        // Initialize the thread pool with the specified size
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {id} got a job; executing.");
+
+            job();
+        });
+
+        Worker { id, thread }
+    }
+}
